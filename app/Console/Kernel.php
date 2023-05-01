cba61a423cd86b75244f1e85e7b343c95c8eadca
redenv <redenv@careindeed.com>
2023-05-01 08:28:39 +0800

    added github workflow

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,16 +8,9 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
 
-    protected $commands = [
-        Commands\EndOfDayTask::class,
-    ];
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('endofday:task')->dailyAt('23:59');
+
     }
 
     /**
